97f8b17312b09cd2aa14f7f82dd9d1d37ebdbd6b
Your Name <you@example.com>
2022-10-23 18:02:39 +0400

    решили 3 задачу

--- Program.cs
+++ Program.cs
@@ -5,14 +5,7 @@
         if (number1 == number2 * number2)
             Console.WriteLine($"Квадрат {number2} равен {number1}");
         else
-            Console.WriteLine("нет");
-
-                   
-Console.ReadLine(number);
-if number=1
-Console.WriteLine("понедельник");
-if number=2  Console.WriteLine("вторник"); 
-
+            Console.WriteLine("нет");         
 
 int number1 = int.Parse(Console.ReadLine());
 int number2 = int.Parse(Console.ReadLine());
@@ -21,6 +14,26 @@ if (koren == number2){
     Console.WriteLine($"Квадрат {number2} равен {number1}");}
 else{
     Console.WriteLine("нет");}
+
+int number = int.Parse(Console.ReadLine());
+if (number == 1) Console.WriteLine("понедельник");
+else if (number == 2)  Console.WriteLine("вторник"); 
+else if (number == 3)  Console.WriteLine("среда"); 
+else if (number == 4)  Console.WriteLine("Чт");
+else if (number == 5)  Console.WriteLine("Пт");
+else if (number == 6)  Console.WriteLine("Сб");
+else if (number == 7)  Console.WriteLine("Вс");
+else Console.WriteLine("такого дня недели нет");
+
+int N = int.Parse(Console.ReadLine());
+  int M = -N;
+  while (M!=N){
+    Console.WriteLine(M);
+    M+=1;
+    }
+Console.WriteLine(M);
 */
 
-  
+
+    
+
