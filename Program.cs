19561e3dea1e4c82a3d650287a2b96dd9692168a
Your Name <you@example.com>
2022-10-23 18:48:31 +0400

    решили задачу с нахождением суммы чисел целого числа

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача1
+Console.WriteLine("Введите любое целое число");
+int N = int.Parse(Console.ReadLine());
+int temp = 0;
+int sum = 0;
+while (N!=0)
+{
+    temp = N%10;
+    sum = sum +temp;
+    N=N/10;
+}
+Console.WriteLine(sum);
+
+
+
+
