c18b174825e463fefdb189d591f214cfaf47b1fc
Your Name <you@example.com>
2022-10-23 20:33:42 +0400

    решили задачу с нахождением max значение их трёх чисел

--- Program.cs
+++ Program.cs
@@ -1,8 +1,19 @@
-﻿// 1.задача с нахождением max и min значение их двух чисел
-
+﻿/* 1.задача с нахождением max и min значение их двух чисел
 Console.WriteLine("Введите два целых числа");
 int N = int.Parse(Console.ReadLine());
 int M = int.Parse(Console.ReadLine());
 if (N>M) Console.WriteLine($"max={N}, min={M}");
 else if (N<M) Console.WriteLine($"max={M}, min={N}");
 else if (N==M) Console.WriteLine("числа равны");
+*/
+
+// 2. максимальное из трёх чисел
+
+Console.WriteLine("Введите три целых числа");
+int N = int.Parse(Console.ReadLine());
+int M = int.Parse(Console.ReadLine());
+int P = int.Parse(Console.ReadLine());
+int max=N;
+if (P>max) max=P;
+if (M>max) max=M;
+Console.WriteLine(max);
