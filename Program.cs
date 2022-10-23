421b979104834a1df9a06ad0322433054602efe3
Your Name <you@example.com>
2022-10-23 17:24:07 +0400

    решили 2 задачу

--- Program.cs
+++ Program.cs
@@ -1,5 +1,26 @@
-﻿Console.WriteLine("Введите имя: ");
-Console.WriteLine("Введите число: ");
-string name = Console.ReadLine();
-int number = int.Parse(Console.ReadLine());
-Console.WriteLine($"{name}, квадрат {number} равен {number*number}");
+﻿
+       /* Console.WriteLine("Введите два числа");
+        int number1 = int.Parse(Console.ReadLine());
+        int number2 = int.Parse(Console.ReadLine());
+        if (number1 == number2 * number2)
+            Console.WriteLine($"Квадрат {number2} равен {number1}");
+        else
+            Console.WriteLine("нет");
+
+                   
+Console.ReadLine(number);
+if number=1
+Console.WriteLine("понедельник");
+if number=2  Console.WriteLine("вторник"); 
+
+
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+int koren = Convert.ToInt32(Math.Sqrt(number1));
+if (koren == number2){
+    Console.WriteLine($"Квадрат {number2} равен {number1}");}
+else{
+    Console.WriteLine("нет");}
+*/
+
+  
