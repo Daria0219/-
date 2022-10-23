921915731bb0eb7899cc7dae839d9f5b7b39d234
Your Name <you@example.com>
2022-10-23 20:46:08 +0400

    решили задачу на определение, является ли число чётным

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿/* 1.задача с нахождением max и min значение их двух чисел
+﻿/* 1. задача с нахождением max и min значение их двух чисел
 Console.WriteLine("Введите два целых числа");
 int N = int.Parse(Console.ReadLine());
 int M = int.Parse(Console.ReadLine());
@@ -7,8 +7,7 @@ else if (N<M) Console.WriteLine($"max={M}, min={N}");
 else if (N==M) Console.WriteLine("числа равны");
 */
 
-// 2. максимальное из трёх чисел
-
+/* 2. максимальное из трёх чисел
 Console.WriteLine("Введите три целых числа");
 int N = int.Parse(Console.ReadLine());
 int M = int.Parse(Console.ReadLine());
@@ -17,3 +16,11 @@ int max=N;
 if (P>max) max=P;
 if (M>max) max=M;
 Console.WriteLine(max);
+*/
+
+// 3. задача на определение, является ли число чётным
+Console.WriteLine("Введите целое число");
+int N = int.Parse(Console.ReadLine());
+int ostatok=N%2;
+if (ostatok==0) Console.WriteLine("чисто чётное");
+else Console.WriteLine("число нечётное");
